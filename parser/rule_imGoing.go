89e89925514cecd77fa77105420d0cddbd95b6c7
Victor <theonlypwner@users.noreply.github.com>
2016-12-10 21:39:40 -0700

    Improve coverage

--- parser/rule_imGoing.go
+++ parser/rule_imGoing.go
@@ -1,9 +1,5 @@
 package parser
 
-import (
-	"victorz.ca/grammar/sequence"
-)
-
 /*
 Keyword: going
 Src: (im|iM|Im) _
@@ -11,7 +7,9 @@ Dst: [I'm] _
 Removed: There is an {IM going} to you!
 */
 
+/*
 func (r *ruleMatcher) rule_imGoing(cur *sequence.Word) {
 	// Disabled
 	return
 }
+*/
